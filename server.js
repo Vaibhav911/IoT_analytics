8ce279d62baeb5666bd50ced23dbf49bfd06416a
Vaibhav Yadav <yadav.vaibhav46@yahoo.com>
2019-05-30 12:26:26 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var app = express();
 var cors = require('cors');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var {Year_Schema} = require('./IoT_Data_Schema.js/index.js');
+// var Campus_Schema = require('./IoT_Campus_Schema.js');
 
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -15,6 +17,32 @@ app.use('/hello', (req, res) => {
 
 app.use('/', (req, res) => {
     console.log("hello");
+    // var year_data = new Year_Schema({
+    //     sensorId: 2,
+    //     year: 2019,
+    //     // monthArray: new Month_Schema({month: 'jan'})
+    // });
+    // year_data.save(err => {
+    //     if (err){
+    //         // res.type('html').status(500);
+    //         // res.write('some error');
+    //         console.log('some error');
+    //     }
+    //     else{
+    //         console.log('object saved sucessfuly');
+    //     }
+    // })
+    var obj = new Year_Schema({sensorId: 2, year: 1234});
+    obj.save(err => {
+        if (err)
+        {
+            console.log('some error');
+        }
+        else
+        {
+            console.log('obj saved successfuly');
+        }
+    })
     res.send('hello');
 });
 
