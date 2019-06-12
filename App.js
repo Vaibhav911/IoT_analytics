bad8e41b25875bc91af614af12e4af21df018465
SuhasShanbhogue <42679731+SuhasShanbhogue@users.noreply.github.com>
2019-06-12 16:04:17 +0530

    Final changes of Phase1

--- App.js
+++ App.js
@@ -1,64 +1,41 @@
 import React, { Component } from 'react';
 import './App.css';
-import axios from 'axios'
+//import axios from 'axios'
 import Chart from './Chart';
+//import Form from './Form';
 
 class App extends Component {
   constructor(){
     super();
     this.state = {
-      chartData:{}
+                    
     }
   }
-
-  componentDidMount() {
-    this.getChartData();
-  }
-
-  getChartData() {
-    axios.get( "http://localhost:5000/graphtesting").then(res => {
-        let labels = [];
-        let data = [];
-        let backgroundColor = [];
-        var t_no = res.data.temperatures.length;
-        console.log(t_no);
-        for (var i=0;i<t_no;i++)
-            {
-              labels.push(res.data.labels[i]);
-              data.push(res.data.temperatures[i]);
-              var red = Math.floor(Math.random()*256);
-              var blue = Math.floor(Math.random()*256);
-              var green = Math.floor(Math.random()*256);
-              backgroundColor.push("rgba("+red+", "+blue+", "+green+", 0.6)");
-            } 
-
-      //  console.log(backgroundColor);
-        this.setState({
-          chartData: {
-            labels:labels,
-            datasets: [
-              {
-                label: "Temperature",
-                data: data,
-                backgroundColor: backgroundColor,
-              }
-            ]
-          }
-        });
-      });
-    }
+  
 
   render(){
 
         return (
+          // <div className='formFromClient'>
+          //   <Form>
+
+          //   </Form>
+
+          // </div>
           <div className="App">
-          {Object.keys(this.state.chartData).length &&
+            
+            
+          
+           
+          {/* {Object.keys(this.state.chartData).length && */}
+            
+            
             <Chart
-              chartData={this.state.chartData}
-              location="temperature"
-              legendPosition="bottom"
+            //   chartData={this.state.chartData}
+            //   location="temperature"
+            //   legendPosition="bottom"
             />
-          }
+          {/* } */}
           </div>
         );
 
