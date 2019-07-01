536be9e124e55d1c8e3099033ec91781132ed019
yashc10 <51187785+yashc10@users.noreply.github.com>
2019-07-01 10:58:03 +0530

    Update and rename IoT_Data_Schema_alt.js to Sensor_Attr_Schema.js

---
+++ Sensor_Attr_Schema.js
@@ -0,0 +1,13 @@
+var mongoose = require ("mongoose");
+
+mongoose.connect( "mongodb+srv://vaibhav:vaibhav@cluster0-txbx7.mongodb.net/test?retryWrites=true");
+
+var Schema = mongoose.Schema;
+
+var Sensor_Attributes_Schema = new Schema({
+    type : { type: String, required: true, unique: true},
+    attributeArray : [String]
+});
+
+const Sensor_Attributes = mongoose.model("Sensor_Attributes_Schema",Sensor_Attributes_Schema);
+module.exports = {Sensor_Attributes_Schema: Sensor_Attributes};
