45a6c57baaaac00e019a391c2f15f8ccdff9c828
Vaibhav Yadav <35480736+Vaibhav911@users.noreply.github.com>
2019-07-01 15:50:04 +0530

    Update read_data.py

--- read_data.py
+++ read_data.py
@@ -158,3 +158,163 @@ def main():
 #start process
 if __name__ == '__main__':
     main()
+    
+    
+    
+    
+    
+ ---------------------------------------
+
+import sys, json, numpy as np
+import pandas as pd
+import datetime
+from dateutil import parser
+from dateutil import relativedelta
+#Read data from stdin
+def read_in():
+    lines = sys.stdin.readlines()
+    #Since our input would only be having one line, parse our JSON data from that
+    return json.loads(lines[0])
+
+def main():
+    #get our data as an array from read_in()
+    data = read_in()
+    freq = data[0]
+    start_time = parser.parse(data[1][0:-1])
+    end_time = parser.parse(data[2][0:-1])
+    annual_data = data[3]
+    monthly_data = data[4]
+    daily_data = data[5]
+    hourly_data = data[6]
+    attribute = 'temperature'
+    
+#    print('frequency:', freq)
+#    print('start time:', start_time)
+#    print('end time:', end_time)
+#    print('annual data', annual_data)
+#    print('monthly data', monthly_data)
+#    print('daily_data', daily_data)
+#    print('hourly data len', len(hourly_data))
+#    print('hourly data', hourly_data)
+
+    if freq == 'annual':
+        print('in annual')
+        num_of_years = end_time.year - start_time.year + 1
+        final_annual_data = [[] for year_num in range(num_of_years)]
+        final_annual_labels = [None for year_num in range(num_of_years)]
+        for year in annual_data:
+            index = (year['year'] -100 + 2000) - start_time.year#change it later
+            for month in  year['monthArray']:
+                for day in month['dayArray']:
+                    for hour in day['hourArray']:
+                        for reading in hour['readingArray']:
+                            final_annual_data[index].append(reading[attribute])
+            final_annual_labels[index] = year['year'] - 100 + 2000
+                  
+
+        for month in monthly_data:
+            index = (month['year'] - 100 + 2000) - start_time.year
+            for day in month['dayArray']:
+                for hour in day['hourArray']:
+                    for reading in hour['readingArray']:
+                        final_annual_data[index].append(reading[attribute])
+            final_annual_labels[index] = month['year']
+        
+        
+        for day in daily_data:
+            index = (day['year'] - 100 + 2000) - start_time.year
+            for hour in day['hourArray']:
+                for reading in hour['readingArray']:
+                    final_annual_data[index].append(reading[attribute])
+            final_annual_labels[index] = day['year']
+
+
+        for hour in hourly_data:
+            index = (hour['year'] - 100 + 2000) - start_time.year
+            for reading in hour['readingArray']:
+                final_annual_data[index].append(reading[attribute])
+            final_annual_labels[index] = hour['year']
+        
+        print('final annual data', final_annual_data)
+        print('final labels array', final_annual_labels)
+        print('mean is ' + str(np.array(final_annual_data).mean(axis=1)))
+   
+
+
+
+
+     
+    if freq == 'monthly':
+        start_time = pd.Timestamp(data[1][0:-1])
+        end_time = pd.Timestamp(data[2][0:-1])
+        num_of_months = (end_time.to_period('M') - start_time.to_period('M')).n + 1
+        final_monthly_data = [[] for month_num in range(num_of_months)]
+        final_monthly_labels = [None for month_num in range(num_of_months)]
+        print('annual array len', len(annual_data))
+        print('monthly array len', len(monthly_data))
+        print('daily array len', len(daily_data))
+        print('hourly array len', len(hourly_data))
+        for year in annual_data:
+            for month in year['monthArray']:
+                    current_time = pd.Timestamp(year['year'] + 1900, int(month['month']), 1)
+                    index = (current_time.to_period('M') - start_time.to_period('M')).n
+                    if index in range(num_of_months):
+                        for day in month['dayArray']:
+                            for hour in day['hourArray']:
+                                for reading in hour['readingArray']:
+                                    final_monthly_data[index].append(reading[attribute])
+                        final_monthly_labels[index] = str(current_time.to_period('M'))
+                    
+
+
+        for month in monthly_data:
+            current_time = pd.Timestamp(month['year'] + 1900, int(month['month']), 1)
+            index = (current_time.to_period('M') - start_time.to_period('M')).n
+            if index in range(num_of_months):
+                for day in month['dayArray']:
+                    for hour in day['hourArray']:
+                        for reading in hour['readingArray']:
+                            final_monthly_data[index].append(reading[attribute])
+                final_monthly_labels[index] = str(current_time.to_period('M'))
+            
+        print('start time', start_time)
+                
+        for day in daily_data:
+#            print('daily obj', day)
+            current_time = pd.Timestamp(day['year']+1900, day['month']+1, day['date'])
+            index = (current_time.to_period('M') - start_time.to_period('M')).n
+            print('calculated index', index, 'current time', current_time)
+            if index in range(num_of_months):
+                for hour in day['hourArray']:
+                    for reading in hour['readingArray']:
+                        final_monthly_data[index].append(reading[attribute])
+                final_monthly_labels[index] = str(current_time.to_period('M'))
+            
+        
+        for hour in hourly_data:
+            current_time = pd.Timestamp(hour['year'] + 1900, hour['month'], hour['date'])
+            index = (current_time.to_period('M') - start_time.to_period('M')).n
+            if index in range(num_of_months):
+                for reading in hour['readingArray']:
+                    final_monthly_labels[index].append(reading[attribute])
+                final_monthly_labels[index] = str(current_time.to_period('M'))
+            
+        print('final monthly data', final_monthly_data)
+        print('final monthly labels', final_monthly_labels)
+        
+        print('means is', np.array(final_monthly_data).mean(axis = 1))
+            
+                    
+        
+#        
+        
+            
+        
+        
+
+#start process
+if __name__ == '__main__': 
+    main()
+    
+    
+    
