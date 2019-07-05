b4796067295db1cfe5931cfcb3d1a6e223e9226f
Vaibhav Yadav <35480736+Vaibhav911@users.noreply.github.com>
2019-07-05 16:11:55 +0530

    Update simulation.js

--- simulation.js
+++ simulation.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-simulate();
+simulate_copy();
 
 // app.use('/test', (req, res) => {
 function simulate()
@@ -24,7 +24,7 @@ var dt = new Date(Date.now());
 // dt.setHours( dt.getHours() + 9);
 var createObjs = setInterval(function(){
     dt.setMinutes( dt.getMinutes() + 15);
-    var sensorIdToPass = 1 + i%2;
+    var sensorIdToPass = 1 /*+ i%2*/  ;
     var humidityToPass = Math.floor(Math.random()*3) + 78;
     var temperatureToPass = Math.floor(Math.random()*13) + 21;
     var luminosityToPass = Math.floor(Math.random()*23001) + 78;
@@ -45,7 +45,7 @@ var createObjs = setInterval(function(){
 
 
     i = i + 1;
-    if(i==200) {
+    if(i==100) {
         // res.send('***simulation finished.***');
         clearInterval(createObjs);
         return;
@@ -53,6 +53,44 @@ var createObjs = setInterval(function(){
 }
 // });
 
+
+function simulate_copy()
+{
+    var i=0;
+var dt = new Date(Date.now());
+// dt.setHours(17);
+// dt.setMonth(11);
+// dt.setDate(31);
+// dt.setHours(dt.getHours() + 3);
+var createObjs = setInterval(function(){
+    dt.setMinutes( dt.getMinutes() + 30);
+    var sensorIdToPass = 2;
+    var humidityToPass = Math.floor(Math.random()*30) + 50;
+    var temperatureToPass = (sensorIdToPass==3)? null : Math.floor(Math.random()*40) + 7;
+    var luminosityToPass = Math.floor(Math.random()*23001);
+
+    // if (!(i > 30 && i < 50))
+    {
+        var link = 'http://localhost:3000/storereading?'
+        + 'sensorId=' + sensorIdToPass
+        + '&temperature=' + temperatureToPass
+        + '&luminosity=' + luminosityToPass
+        + '&humidity=' + humidityToPass
+        + '&i=' + i
+        + '&timeStamp=' + dt;
+
+    //  console.log(i + 'timestamp: ' + dt)
+    axios.post(link).then(res => {
+        console.log(i + 'timestamp: ' + dt)
+        console.log('link: ' + link);
+    });
+
+    }
+    i = i + 1;
+    if(i==90*24) {
+        clearInterval(createObjs);
+}}, 5000);
+}
 app.listen(5000, () => {
     console.log('listening at 5000');
 })
